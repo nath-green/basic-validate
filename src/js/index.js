a1bc6eb038e8139dc16726259f5e967022bb4dcf
Nathan Green <nathan.green@jet2.com>
2018-07-26 14:49:09 +0100

    prep for npm package

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import * as Validate from './Validate'
-
-console.log(Validate.isEmpty(''))
-console.log(Validate.charsBetween('123', 1, 3))
-console.log(Validate.isEmail('hello@nathgreen.co.uk'))
-console.log(Validate.isEqual('password', 'password123'))
-console.log(Validate.isTelephone('07712'))
