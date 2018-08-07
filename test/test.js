0a33817570cf330bc99661d90b14d25109fe70b9
Nathan Green <nathan.green@jet2.com>
2018-08-07 08:45:53 +0100

    added tests for all functions

--- test/test.js
+++ test/test.js
@@ -12,3 +12,63 @@ describe('isEmpty', () => {
     assert.equal(result, false)
   })
 })
+
+describe('isZero', () => {
+  it('Value is zero', () => {
+    let result = Validate.isZero(0)
+    assert.equal(result, true)
+  })
+
+  it('Value is not zero', () => {
+    let result = Validate.isZero(12)
+    assert.equal(result, false)
+  })
+})
+
+describe('charsBetween', () => {
+  it('Characters within min & max', () => {
+    let result = Validate.charsBetween('hello', 1, 5)
+    assert.equal(result, true)
+  })
+
+  it('Characters outside min & max', () => {
+    let result = Validate.charsBetween('hello', 2, 4)
+    assert.equal(result, false)
+  })
+})
+
+describe('isEmail', () => {
+  it('Email is valid', () => {
+    let result = Validate.isEmail('hello@nathgreen.co.uk')
+    assert.equal(result, true)
+  })
+
+  it('Email is invalid', () => {
+    let result = Validate.isEmail('hello@')
+    assert.equal(result, false)
+  })
+})
+
+describe('isTelephone', () => {
+  it('Telephone is valid', () => {
+    let result = Validate.isTelephone('077123456789')
+    assert.equal(result, true)
+  })
+
+  it('Telephone is invalid', () => {
+    let result = Validate.isTelephone('123')
+    assert.equal(result, false)
+  })
+})
+
+describe('isEqual', () => {
+  it('Params are equal', () => {
+    let result = Validate.isEqual('password', 'password')
+    assert.equal(result, true)
+  })
+
+  it('Params not equal', () => {
+    let result = Validate.isEqual('password', 'passw0rd')
+    assert.equal(result, false)
+  })
+})
